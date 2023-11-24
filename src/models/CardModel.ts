0a3873036f52df4e7d33a8f85af4c1dc0a61ebb4
Conor <blackripple13@gmail.com>
2023-11-24 14:33:24 +0000

    Design a first draft of the card model

--- src/models/CardModel.ts
+++ src/models/CardModel.ts
@@ -1,16 +1,51 @@
+interface Card {
+    id: string;
+    title: string;
+    sizes: string[];
+    basePrice: number;
+    pages: {
+      title: string;
+      templateId: string;
+    }[];
+  }
+
+  interface Sizes {
+    id: string;
+    title: string;
+    priceMultiplier: number;
+  }
+
+  interface Template {
+    id: string;
+    width: number;
+    height: number;
+    imageUrl: string;
+  }
 
 
 class CardModel {
-    private cardsData: any;
-    private sizesData: any;
-    private templateData: any;
+    private cardsData: Card[];
+    private sizesData: Sizes[];
+    private templateData: Template[];
 
-    constructor(cardsData: any, sizesData: any, templateData: any) {
+    constructor(cardsData: Card[], sizesData: Sizes[], templateData: Template[]) {
         this.cardsData = cardsData;
         this.sizesData = sizesData;
         this.templateData = templateData;
     }
 
+    getCardImageUrl(templateId: string): string {
+        let template = this.templateData.find(template => template.id === templateId);
+        return template ? template.imageUrl : '';
+    }
+
+    getAllCards(): { title: string; imageUrl: string; url: string }[] {
+        return this.cardsData.map(card => ({
+          title: card.title,
+          imageUrl: this.getCardImageUrl(card.pages[0].templateId),
+          url: `/cards/${card.id}`
+        }));
+      }
 
 }
 
