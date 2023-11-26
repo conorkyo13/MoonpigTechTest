da2c719e5d8d966f7643cf1cca086108d0b50a9f
Conor <blackripple13@gmail.com>
2023-11-26 19:12:15 +0000

    Implement /cards/ endpoint

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,47 @@
 import * as express from "express";
+import jsonFetcher from "./jsonFetcher/jsonFetcher";
+import CardModel from "./models/CardModel";
+import CardView from "./views/CardView";
+import CardController from "./controllers/CardController";
+
+const cardsURL = 'https://moonpig.github.io/tech-test-node-backend/cards.json';
+const sizesURL = 'https://moonpig.github.io/tech-test-node-backend/sizes.json';
+const templatesURL = 'https://moonpig.github.io/tech-test-node-backend/templates.json';
 
 export const app = express()
-
 app.set('json spaces', 2);
 
-app.get('/cards', async () => {
-  // respond with a list of cards
-})
+async function fetchDataAndInitiateApp() {
+  try {
+    const cardsData = new jsonFetcher(cardsURL);
+    const sizesData = new jsonFetcher(sizesURL);
+    const templatesData = new jsonFetcher(templatesURL);
 
-app.get('/cards/:cardId/:sizeId?', () => {
-  // respond with card by id
-})
+    await Promise.all([
+      cardsData.fetchData(),
+      sizesData.fetchData(),
+      templatesData.fetchData()
+    ]);
+
+    const cardModel = new CardModel(cardsData.getData(), sizesData.getData(), templatesData.getData());
+    const cardView = new CardView();
+    const cardController = new CardController(cardModel, cardView);
+
+    app.get('/cards', async (req, res) => {
+      // respond with a list of cards
+        cardController.getCards(req,res);
+    });
+
+    app.get('/cards/:cardId/:sizeId?', async (req, res) => {
+      // respond with card by id
+      // You can use cardController.getCardById(req.params.cardId, req.params.sizeId) here to handle the request
+    });
+
+  } catch(error) {
+  
+    console.log(error);
+    throw(error);
+  }
+}
+
+fetchDataAndInitiateApp();
