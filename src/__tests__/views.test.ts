67996350d601471676f56d3cefc7f5fb8e9e1eaf
Conor <blackripple13@gmail.com>
2023-11-27 12:47:12 +0000

    Add unit tests for view model

--- src/__tests__/views.test.ts
+++ src/__tests__/views.test.ts
@@ -1,1 +1,21 @@
 import * as request from 'supertest'
+import CardView from '../views/CardView';
+
+describe('View Model', () => {
+
+    const viewModel = new CardView();
+
+    test('it should format the cards correctly', () => {
+        const cardsData = [
+            { id: '1', title: 'Card 1', sizes: ['s', 'm', 'l'], basePrice: 200, pages: [{ title: "front cover", templateId: "template1"}] },
+            { id: '2', title: 'Card 2', sizes: ['s', 'm'], basePrice: 300, pages: [{ title: "back cover", templateId: "template2"}] },
+            
+        ];
+        
+        let formattedCards = viewModel.formatCards(cardsData);
+        //expect(formattedCards).toHaveLength(3);
+        expect(formattedCards[0]).toHaveProperty('title');
+        expect(formattedCards[0]).toHaveProperty('imageUrl');
+        expect(formattedCards[0]).toHaveProperty('url');
+    })
+})
